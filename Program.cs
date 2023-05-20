27f6c81a55915bb07dd28c70db97a7089d6c1c3f
ttmicku <wun002002@gmail.com>
2023-05-20 12:44:48 +0700

    array1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program {
+    static void Main(string[] args) {
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+
+        string[] PeopleInput = Console.ReadLine().Split(' ');
+        int[] People = Array.ConvertAll(PeopleInput, int.Parse);
+        int maxCustomers = 0;
+
+        for (int i = 0; i <= N - K; i++) {
+            int Customers = 0;
+
+        for (int j = i - K; j <= i + K; j++) {
+            if (j >= 0 && j < N) {
+                    Customers += People[j]; }
+    }
+            maxCustomers = Math.Max(maxCustomers, Customers);
+ }
+        Console.WriteLine(maxCustomers);
+    }
+}
